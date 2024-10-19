f375e67698a7e092195bfc26b2d3dffded4ec1b7
Kwandokuhle11 <lwethukwando23@gmail.com>
2024-10-19 20:28:25 +0200

    library management system

--- Documents/library_management_system/library/models.py
+++ Documents/library_management_system/library/models.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from django.db import models
 
 # Create your models here.
@@ -7,8 +8,8 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13, unique=True)
-    published_date = models.DateField()
-    copies_available = models.PositiveIntegerField()
+    published_date = models.DateField(default=timezone.now)
+    copies_available = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
